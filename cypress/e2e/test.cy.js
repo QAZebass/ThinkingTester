b9e91875a2fd6507c42020aa3d75227b0ad2f966
QAZebass <ze.basszender@gmail.com>
2023-09-05 19:10:12 -0300

    ThinkingTest | Faker installed. Sample test created

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,17 @@
 describe('testing',()=>{
     it('test',()=>{
-        cy.visit('/')
+        cy.request({
+            'method': 'POST',
+            'url': 'https://thinking-tester-contact-list.herokuapp.com/users',
+            failOnStatusCode: false,
+            body:{
+                "firstName": "Test",
+                "lastName": "User",
+                "email": "testingg@fake.com",
+                "password": "myPassword"
+            }   
+        }).then(response=>{
+            cy.log(response)
+        })
     })
 })
